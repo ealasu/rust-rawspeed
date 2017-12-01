99c5afdfc86353c18f043c74369d4ef90ebd0bc7
eman <e@m.an>
2017-12-01 10:18:30 -0700

    c

--- build.rs
+++ build.rs
@@ -1,9 +1,24 @@
-// build.rs
-
 extern crate cc;
+extern crate cmake;
+
+use std::env;
 
 fn main() {
+    let mut pugixml_path = env::current_dir().unwrap();
+    pugixml_path.push("pugixml");
+    let rawspeed_dst = cmake::Config::new("rawspeed")
+        .define("BUILD_TESTING", "OFF")
+        .define("BUILD_FUZZERS", "OFF")
+        .define("WITH_OPENMP", "OFF")
+        .define("USE_BUNDLED_PUGIXML", "ON")
+        .define("PUGIXML_PATH", pugixml_path)
+        .build();
+    println!("cargo:rustc-link-search=native={}", rawspeed_dst.display());
+    println!("cargo:rustc-link-lib=static=rawspeed");
+
     cc::Build::new()
-        .file("foo.c")
-        .compile("foo");
+        .cpp(true)
+        .include("rawspeed/src/librawspeed")
+        .file("rawspeed_interop.cpp")
+        .compile("rawspeed_interop");
 }
---
+++ rawspeed_interop.cpp
@@ -0,0 +1,5 @@
+#include "RawSpeed-API.h";
+
+void do_stuff() {
+}
+
