a624a97f2a682fb4e434a9817bab96ff040ec723
eman <e@m.an>
2017-12-03 00:25:19 +0000

    c

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
 extern crate libc;
+extern crate rawspeed_sys;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+use std::ffi::CString;
+use std::path::Path;
+use std::os::raw::c_void;
+use rawspeed_sys::*;
+
+pub struct CameraMetadata(*const c_void);
+
+impl CameraMetadata {
+    pub fn init(filename: &str) -> Self {
+        let filename = CString::new(filename).unwrap();
+        let ptr = unsafe { rawspeed_metadata_init(filename.as_ptr()) };
+        if ptr.is_null() {
+            None
+        }
+        CameraMetadata(ptr)
+    }
+}
